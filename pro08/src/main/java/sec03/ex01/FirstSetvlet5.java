c540bfbe54b3fd342f3a2ede8b6942d24b0029c8
Jyebin <yebin0322@naver.com>
2024-05-06 01:13:52 +0900

    복습

---
+++ pro08/src/main/java/sec03/ex01/FirstSetvlet5.java
@@ -0,0 +1,13 @@
+package sec03.ex01;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class FirstSetvlet5
+ */
+@WebServlet("/first5")
+public class FirstSetvlet5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
