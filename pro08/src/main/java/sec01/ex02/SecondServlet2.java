4ab1e8ec51a63b13f3f4ed28af1133e070d1b40d
Jyebin <yebin0322@naver.com>
2024-05-05 22:11:29 +0900

    복습

---
+++ pro08/src/main/java/sec01/ex02/SecondServlet2.java
@@ -0,0 +1,22 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/second2") //브라우저에서 재요청하면 브라우저로 메세지를 출력하는 서블릿
+public class SecondServlet2 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("sendRedirect를 이용한 redirect 실습입니다.");
+		out.println("</body></html>");
+	}
+}
---
+++ pro08/src/main/java/sec01/ex03/SecondServlet3.java
@@ -0,0 +1,22 @@
+package sec01.ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/second3")
+public class SecondServlet3 extends HttpServlet { //브라우저에서 재요청하면 브라우저로 메세지 출력
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("sendRedirect를 이용한 redirect 실습입니다.");
+		out.println("</body></html>");
+	}
+}
